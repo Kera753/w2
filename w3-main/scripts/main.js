46c300e48cfd1a1c0093553a47ce1ce32e4d28f8
Kera753 <87632474+Kera753@users.noreply.github.com>
2021-12-02 11:26:15 +0900

    Update main.js

--- w3-main/scripts/main.js
+++ w3-main/scripts/main.js
@@ -1,6 +1,10 @@
 // Image switcher code
 
-let myImage = document.querySelector('img');
+let myImage = document.querySelector('img'); // オブジェクトを代入
+//documentは組み込み式のオブジェクト
+//ブラウザが表示しているページ、DOHとも呼ぶ。Document Object Modelの略。
+//querySelectorはページの一部(HTML要素)を選ぶ関数。
+
 
 myImage.onclick = function() {
   let mySrc = myImage.getAttribute('src');
@@ -18,10 +22,12 @@ let myHeading = document.querySelector('h1');
 
 function setUserName() {
   let myName = prompt('Please enter your name.');
+  //promptは組み込み式の関数。 prompt Windowを表示して、ユーザ入力をさせる。
   if(!myName) {
     setUserName();
   } else {
     localStorage.setItem('name', myName);
+    // 入力データを特別な場所に保存する
     myHeading.innerHTML = 'Mozilla is cool, ' + myName;
   }
 }
@@ -33,6 +39,6 @@ if(!localStorage.getItem('name')) {
   myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
 }
 
-myButton.onclick = function() {
-  setUserName();
+myButton.onclick = function() { // ボタンをクリックしたら
+  setUserName(); 
 }
